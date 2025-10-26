be5ebbecea18d20b0b58452c1bd8142561358f37
Chaos Zhou <672112429@qq.com>
2025-10-26 11:41:23 +0800

    merge Project2ErrorListener

--- src/main/java/impl/project2/Project2ErrorListener.java
+++ src/main/java/impl/project2/Project2ErrorListener.java
@@ -2,10 +2,12 @@ package impl.project2;
 
 import framework.project2.Grader;
 import framework.project2.MissingSymbolError;
-import org.antlr.v4.runtime.BaseErrorListener;
-import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.ATN;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Project2ErrorListener extends BaseErrorListener {
     private final Grader grader;
@@ -15,9 +17,55 @@ public class Project2ErrorListener extends BaseErrorListener {
 
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-        // TODO: extract information
-        MissingSymbolError missingSymbol = new MissingSymbolError("qwq", 1234);
+        Matcher matcher = Pattern.compile("missing '([^']+)'", Pattern.CASE_INSENSITIVE).matcher(msg);
+        String missingSymbolName = null;
+        if (!matcher.find()) {
+            throw new ParseCancellationException();
+        }
 
+        String missing = "'" + matcher.group(1) + "'";
+
+        // 由msg中的符号得到其名称, ';' -> 'SEMI'
+        String symbolicName = null;
+        if (recognizer instanceof Parser parser) {
+            Vocabulary vocab = parser.getVocabulary();
+            ATN atn = parser.getATN();
+            int maxType = atn.maxTokenType;
+            for (int t = 0; t <= maxType; t++) {
+                String lit = vocab.getLiteralName(t);
+                if (lit != null && lit.equals(missing)) {
+                    symbolicName = vocab.getSymbolicName(t);
+                    break;
+                }
+            }
+        }
+
+        // 行号提取
+        int resultLine = 0;
+        try {
+            if (offendingSymbol instanceof Token token && recognizer instanceof Parser) {
+                TokenStream tokens = ((Parser) recognizer).getInputStream();
+                int idx = token.getTokenIndex();
+                Token prev = null;
+                for (int i = idx - 1; i >= 0; i--) {
+                    Token t = tokens.get(i);
+                    if (t.getType() == Token.EOF) continue;
+                    prev = t;
+                    break;
+                }
+                if (prev != null) {
+                    resultLine = prev.getLine();
+                } else {
+                    resultLine = token.getLine();
+                }
+            } else {
+                resultLine = line;
+            }
+        } catch (Exception ex) {
+            resultLine = line;
+        }
+        // 打印时行数从0开始，与getLine值相差1
+        MissingSymbolError missingSymbol = new MissingSymbolError(symbolicName, resultLine-1);
         this.grader.getWriter().println(missingSymbol);
         throw new ParseCancellationException();
     }
